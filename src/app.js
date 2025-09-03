2312a6b44d0eca394beb6f8d7f354fe0e86cda3d
Aaditya Dev Verma <aditya.nomercy6@gmail.com>
2025-09-03 16:59:56 +0530

    Make Serverless

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import healthCheckRouter from "./routes/healthCheck.routes.js";
 import userRouter from "./routes/user.routes.js";
@@ -10,32 +10,32 @@ import dashboardRouter from "./routes/dashboard.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import videoRouter from "./routes/video.routes.js";
-import {errorHandler} from "./middlewares/error.middlewares.js"
-
+import { errorHandler } from "./middlewares/error.middlewares.js";
+import ServerlessHttp from "serverless-http";
 
 const app = express();
 
 app.use(
-    cors({
-        origin: process.env.CORS_ORIGIN,
-        credentials: true
-    })
-)
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-app.use(express.json({limit:"16kb"}))
+app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"))
-app.use(cookieParser())
-app.use("/api/v1/healthCheck",healthCheckRouter);
-app.use("/api/v1/users",userRouter);
-app.use("/api/v1/comments",commentRouter);
-app.use("/api/v1/likes",likeRouter);
-app.use("/api/v1/playlists",playlistRouter);
-app.use("/api/v1/dashboard",dashboardRouter);
-app.use("/api/v1/subscriptions",subscriptionRouter);
-app.use("/api/v1/tweets",tweetRouter);
-app.use("/api/v1/videos",videoRouter);
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use("/api/v1/healthCheck", healthCheckRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlists", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/videos", videoRouter);
 
 app.use(errorHandler);
 
-export {app}
+export default ServerlessHttp(app);
